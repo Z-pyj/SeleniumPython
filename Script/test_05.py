c8ffc517764e70760dce151133dbc658e8ba6971
zw <pyj.moon@gmail.com>
2021-11-03 15:20:57 +0800

    test

--- Script/test_05.py
+++ Script/test_05.py
@@ -13,5 +13,5 @@ class Test_allure:
     @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
     @allure.step('我是测试步骤001')
     def test_al(self, a):
-        allure.attach('描述', '我是测试步骤001的描述～～～')
+        allure.attach('描述', '我是测试步骤001的描述～～')
         assert a != 2
