f8cdf584f5fa158b15b10bc2d361cdb34fa105c2
zw <pyj.moon@gmail.com>
2021-11-04 17:03:33 +0800

    update

--- Tools/read_data.py
+++ Tools/read_data.py
@@ -14,16 +14,23 @@ class Read_Data:
             print(data)
             return data
 
+    def return_datas(self):
+        with open(self.file_path, 'r', encoding='utf-8') as f:
+            context = yaml.load_all(f, Loader=yaml.FullLoader)
+            for i in context:
+                print(i)
 
-def package_param_data():
-    list_data = []  # 存储参数值列表，格式[(用例编号1,输入内容2),(用例编号1,输入内容2)...]
-    yaml_data = Read_Data("searchData.yml").return_data()  # 返回yaml文件读取数据
-    for i in yaml_data.keys():
-        list_data.append((i, yaml_data.get(i).get('input_text')))  # list_data中添加参数值
-    print(list_data)
-    return list_data
+
+    def package_param_data(self, funcCase, case):
+        list_data = []  # 存储参数值列表，格式[(用例编号1,输入内容2),(用例编号1,输入内容2)...]
+        yaml_data = Read_Data(self.file_path).return_data()[funcCase]  # 返回yaml文件读取数据
+        for i in yaml_data.keys():
+            list_data.append((i, yaml_data.get(i).get(case)))  # list_data中添加参数值
+        print(list_data)
+        return list_data
+
 
 
 if __name__ == '__main__':
     Read_Data("searchData.yml").return_data()
-    package_param_data()
+    # package_param_data("searchData.yml", "search_text", "input_text")
