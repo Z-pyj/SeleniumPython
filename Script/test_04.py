cba6f9a710cd90e92933fae54a5269d6a8082d7a
Zw <pyj.moon@gmail.com>
2021-11-07 15:31:38 +0800

    activity

--- Script/test_04.py
+++ Script/test_04.py
@@ -1,6 +1,6 @@
 import yaml
 
-with open('../Data/searchData.yml', encoding='utf8') as f:
+with open('../Data/searchData2.yml', encoding='utf8') as f:
     # 多组数据使用load_all(),此方法返回一个生成器，需要使用for循环迭代读取每一组数据
     context = yaml.safe_load(f)
     print(context)
--- Tools/read_data.py
+++ Tools/read_data.py
@@ -17,20 +17,31 @@ class Read_Data:
     def return_datas(self):
         with open(self.file_path, 'r', encoding='utf-8') as f:
             context = yaml.load_all(f, Loader=yaml.FullLoader)
-            for i in context:
-                print(i)
+            return context
+            # for i in context:
+            #     if "search_text2" not in i.keys():
+            #         pass
+            #     else:
+            #         print(i["search_text2"]["search_test_002"])
 
-
-    def package_param_data(self, funcCase, case):
+    def package_param_data(self, case):
         list_data = []  # 存储参数值列表，格式[(用例编号1,输入内容2),(用例编号1,输入内容2)...]
-        yaml_data = Read_Data(self.file_path).return_data()[funcCase]  # 返回yaml文件读取数据
+        yaml_data = Read_Data(self.file_path).return_data()  # 返回yaml文件读取数据
+        for i in yaml_data.keys():
+            list_data.append((i, yaml_data.get(i).get(case)))  # list_data中添加参数值
+        print(list_data)
+        return list_data
+
+    def package_param_datas(self, funcCase, case):
+        list_data = []  # 存储参数值列表，格式[(用例编号1,输入内容2),(用例编号1,输入内容2)...]
+        yaml_data = Read_Data(self.file_path).return_datas()[funcCase]  # 返回yaml文件读取数据
         for i in yaml_data.keys():
             list_data.append((i, yaml_data.get(i).get(case)))  # list_data中添加参数值
         print(list_data)
         return list_data
 
 
-
 if __name__ == '__main__':
-    Read_Data("searchData.yml").return_data()
-    # package_param_data("searchData.yml", "search_text", "input_text")
+    # Read_Data("searchData1.yml").return_data()
+    # Read_Data("searchData2.yml").return_datas()
+    Read_Data("searchData2.yml").package_param_data("input_text")
